78904d2f1800f58d6857688a2a762629514d67a2
SeungHak <bsh93367@gmail.com>
2024-07-18 09:44:05 +0900

    config: add vercel/speed-insight

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/global.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <SpeedInsights />;
+    </>
+  );
 }
